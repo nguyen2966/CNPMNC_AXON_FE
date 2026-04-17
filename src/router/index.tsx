579c88cac334e2e01c615077fb4dd0921ba2fc0b
NewtakaNghia <nghia.trantrong0409@hcmut.edu.vn>
2026-04-18 01:01:00 +0700

    fix: Fixing for authentication, prevent for login page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthStore } from '../store/authStore.ts';
+
 import { AppLayout } from '../components/layout/AppLayout.tsx';
 import { PrivateRoute } from '../components/layout/PrivateRoute.tsx';
 import { LoginPage } from '../features/auth/LoginPage.tsx';
@@ -11,7 +13,30 @@ import { DepartmentsPage } from '../features/departments/DepartmentsPage.tsx';
 import { ValidationPage } from '../features/validation/ValidationPage.tsx';
 import { AuditLogsPage } from '../features/auditLogs/auditLogPage.tsx';
 
-export const AppRouter: React.FC = () => (
+export const AppRouter: React.FC = () => {
+  const { initAuth, isInitializing } = useAuthStore();
+
+  useEffect(() => {
+    initAuth();
+  }, [initAuth]);
+
+  if (isInitializing) {
+    return (
+      <div className="flex h-screen w-screen flex-col items-center justify-center bg-[#0a0f1c]">
+        <div className="relative flex items-center justify-center">
+          <div className="absolute h-20 w-20 rounded-full border-4 border-blue-900/20"></div>
+
+          <div className="h-20 w-20 rounded-full border-4 border-transparent border-t-blue-500 border-r-blue-400 animate-spin shadow-[0_0_15px_rgba(59,130,246,0.3)]"></div>
+          
+          <div className="absolute h-8 w-8 rounded-full bg-blue-500/20 animate-pulse"></div>
+        </div>
+        
+        <div className="mt-8 text-blue-400 text-lg font-medium tracking-wider drop-shadow-[0_0_8px_rgba(59,130,246,0.4)] animate-pulse">
+        </div>
+      </div>
+    );
+  }
+  return (
   <BrowserRouter>
     <Routes>
       <Route path="/login" element={<LoginPage />} />
@@ -36,4 +61,5 @@ export const AppRouter: React.FC = () => (
       <Route path="*" element={<Navigate to="/dashboard" replace />} />
     </Routes>
   </BrowserRouter>
-);
+  );
+};
--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,17 +1,21 @@
 import { create } from 'zustand';
 import type { UserInfo } from '../types/user.types';
 import { tokenUtils } from '../utils/token';
+import { authApi } from '../api/auth.api';
 
 interface AuthState {
   user: UserInfo | null;
+  isInitializing: boolean,
   isAuthenticated: boolean;
   setAuth: (user: UserInfo, access: string, refresh: string) => void;
   logout: () => void;
+  initAuth: () => Promise<void>;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
   user: null,
-  isAuthenticated: !!tokenUtils.getAccessToken(),
+  isInitializing: true,
+  isAuthenticated: false,
   setAuth: (user, access, refresh) => {
     tokenUtils.setTokens(access, refresh);
     set({ user, isAuthenticated: true });
@@ -19,5 +23,32 @@ export const useAuthStore = create<AuthState>((set) => ({
   logout: () => {
     tokenUtils.clear();
     set({ user: null, isAuthenticated: false });
+  },
+  initAuth: async () => {
+    const refreshToken = tokenUtils.getRefreshToken();
+
+    if (!refreshToken) {
+      set({ isInitializing: false, isAuthenticated: false, user: null });
+      return;
+    }
+
+    try {
+      const loginRes = await authApi.refresh(refreshToken);
+
+      const user: UserInfo = {
+        id: loginRes.user.id,
+        email: loginRes.user.email,
+        role: loginRes.user.role,
+        fullName: loginRes.user.fullName,
+      }
+
+      tokenUtils.setTokens(loginRes.accessToken, loginRes.refreshToken);
+
+      set({ user, isAuthenticated: true, isInitializing: false });
+    } catch (error) {
+      console.error('Khôi phục phiên đăng nhập thất bại:', error);
+      tokenUtils.clear();
+      set({ user: null, isAuthenticated: false, isInitializing: false });
+    }
   },
 }));
